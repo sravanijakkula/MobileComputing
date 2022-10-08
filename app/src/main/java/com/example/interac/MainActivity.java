0a199c754c08e5b6d2318cddbda4898456cddc56
Padmavathi Maddukuri <98134981+Padmavathi1312@users.noreply.github.com>
2022-10-08 18:41:00 -0500

    Adding .java files
    Added java files for reset 2 screen and calendar screen

--- app/src/main/java/com/example/interac/MainActivity.java
+++ app/src/main/java/com/example/interac/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.interac;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.Months, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
     }
 }
