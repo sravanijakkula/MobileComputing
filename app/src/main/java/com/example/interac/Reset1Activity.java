e74d18d43e26d80a7b66e58621f602954e2e3527
Trilochan Reddy Sama <84437975+Trilochan-Reddy@users.noreply.github.com>
2022-10-08 01:19:44 -0500

    Added Reset1Activity.java file
    adding java file for resetscreen1 Activity

---
+++ app/src/main/java/com/example/interac/Reset1Activity.java
@@ -0,0 +1,18 @@
+package com.example.resetscreen1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Reset1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
+
