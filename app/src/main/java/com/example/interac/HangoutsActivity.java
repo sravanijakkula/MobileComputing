0d39a16dae0327e0023c2d34b9fd1bbf41e3d535
Nikhila212 <97923761+Nikhila212@users.noreply.github.com>
2022-10-08 13:49:09 -0500

    Changes in the profile screen and food courts page created

---
+++ app/src/main/java/com/example/interac/HangoutsActivity.java
@@ -0,0 +1,15 @@
+package com.example.interac;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HangoutsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hangouts);
+
+
+    }
+}
