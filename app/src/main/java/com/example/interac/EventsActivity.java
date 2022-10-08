1c73b096a9b0b39309863b95f2044e7b0ab634df
Trilochan Reddy Sama <84437975+Trilochan-Reddy@users.noreply.github.com>
2022-10-08 09:07:48 -0500

    Added EventsActivity.java file
    The functionality code for reset1 screen is present in Events Activity.java file

---
+++ app/src/main/java/com/example/interac/EventsActivity.java
@@ -0,0 +1,14 @@
+package com.example.interac;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
