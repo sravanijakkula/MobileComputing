d1aac3fad9975c25600e1fc19a575a5cf96e28db
Padmavathi Maddukuri <98134981+Padmavathi1312@users.noreply.github.com>
2022-10-08 19:33:11 -0500

    uploaded java files
    Java files for Launch, reset2 and Academic Calendar

---
+++ app/src/main/java/com/example/interac/AcademicCalendar.java
@@ -0,0 +1,24 @@
+package com.example.interac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class AcademicCalendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_academic_calendar);
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.Months, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
+    }
+}
