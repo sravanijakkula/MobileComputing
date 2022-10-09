9c6d787a99d62498ca7cd27da48a7eb49c0a93f2
Sravani Jakkula <97769309+sravanijakkula@users.noreply.github.com>
2022-10-08 23:57:46 -0500

    Update CampusResourcesActivity.java

--- app/src/main/java/com/example/interac/CampusResourcesActivity.java
+++ app/src/main/java/com/example/interac/CampusResourcesActivity.java
@@ -9,5 +9,14 @@ public class CampusResourcesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campus_resources);
+        ImageView imageView8=(ImageView) findViewById(R.id.imageView8);
+        //ImageView imageViewAboutUs=(ImageView) findViewById(R.id.imageViewAboutus);
+        imageView8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(CampusResourcesActivity.this, DashboardActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
