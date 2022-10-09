89f444dbedc7493b97b8785f57ca426302874801
Nikhila212 <97923761+Nikhila212@users.noreply.github.com>
2022-10-09 00:22:00 -0500

    Changes updated for all

--- app/src/main/java/com/example/interac/LaunchScreen.java
+++ app/src/main/java/com/example/interac/LaunchScreen.java
@@ -2,7 +2,10 @@ package com.example.interac;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LaunchScreen extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class LaunchScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_screen);
+
+        Button button =(Button) findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LaunchScreen.this, LoginActivity.class);
+                startActivity(i);
+            }
+
+        });
+        Button button1 =(Button) findViewById(R.id.button6);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LaunchScreen.this, RegisterActivity.class);
+                startActivity(i);
+            }
+
+        });
     }
 }
