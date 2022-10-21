98d5b43d83c1eb52d3bb9320b68bedf36f71444b
Nikhila212 <97923761+Nikhila212@users.noreply.github.com>
2022-10-21 15:08:42 -0500

    S546518 - Reset3 page activity and java code updation

--- app/src/main/java/com/example/interac/Reset3Activity.java
+++ app/src/main/java/com/example/interac/Reset3Activity.java
@@ -3,6 +3,8 @@ package com.example.interac;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,14 @@ public class Reset3Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
         setContentView(R.layout.activity_reset3);
         Button button9=(Button) findViewById(R.id.button9);
         button9.setOnClickListener(new View.OnClickListener() {
