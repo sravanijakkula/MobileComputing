8e53c9298371417864950712a4208aab6014552a
Padmavathi1312 <padma.maddukuri1@gmail.com>
2022-10-21 21:30:19 -0500

    updated launch screen, reset 2 screen , Academic Calendar , hangouts screen.

--- app/src/main/java/com/example/interac/AcademicCalendar.java
+++ app/src/main/java/com/example/interac/AcademicCalendar.java
@@ -1,30 +1,123 @@
 package com.example.interac;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.content.Intent;
-
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AcademicCalendar extends AppCompatActivity {
+
+    Button b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_academic_calendar);
-        ImageView imageview=(ImageView) findViewById(R.id.imageView18);
+
+        b10 = (Button) findViewById(R.id.button10);
+        b11 = (Button)findViewById(R.id.button11);
+        b12 = (Button)findViewById(R.id.button12);
+        b13 = (Button)findViewById(R.id.button13);
+        b14 = (Button)findViewById(R.id.button14);
+        b15 = (Button)findViewById(R.id.button15);
+        b16 = (Button)findViewById(R.id.button16);
+        b17 = (Button)findViewById(R.id.button17);
+        b18 = (Button)findViewById(R.id.button18);
+        b19 = (Button)findViewById(R.id.button19);
+        b20 =(Button) findViewById(R.id.button20);
+
+
+        b10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                seeEvents();
+            }
+        });
+        b11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                seeEvents();
+            }
+        });
+        b12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                seeEvents();
+            }
+        });
+        b13.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                seeEvents();
+            }
+        });
+        b14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                seeEvents();
+            }
+        });
+        b15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                seeEvents();
+            }
+        });
+        b16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                seeEvents();
+            }
+        });
+        b17.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                seeEvents();
+            }
+        });
+        b18.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                seeEvents();
+            }
+        });
+        b19.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                seeEvents();
+            }
+        });
+        b20.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                seeEvents();
+            }
+        });
+
+
+        ImageView imageview = (ImageView) findViewById(R.id.imageView18);
         imageview.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i=new Intent(AcademicCalendar.this,DashboardActivity.class);
+                Intent i = new Intent(AcademicCalendar.this, DashboardActivity.class);
                 startActivity(i);
             }
         });
+    }
+
+    public void seeEvents() {
+        Intent i = new Intent(AcademicCalendar.this, CalenderView.class);
+        startActivity(i);
+
     }
 }
