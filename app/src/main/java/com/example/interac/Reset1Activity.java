7f373a924014e23939c480b3b09ffa5f20d8bf7e
Trilochan Reddy Sama <84437975+Trilochan-Reddy@users.noreply.github.com>
2022-10-21 23:04:28 -0500

    S546914-Trilochan Reddy Sama-Made changes
     Updated java file for Reset 1 screen

--- app/src/main/java/com/example/interac/Reset1Activity.java
+++ app/src/main/java/com/example/interac/Reset1Activity.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Reset1Activity extends AppCompatActivity {
