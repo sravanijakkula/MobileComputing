3f24602b57b2c74335278f946ea4e7eb79f26340
Sravani Jakkula <97769309+sravanijakkula@users.noreply.github.com>
2022-10-21 22:30:35 -0500

    S546984 - Sravani Jakkula - snapshots updated
    added functionality for snapshots screen

--- app/src/main/java/com/example/interac/SnapshotsActivity.java
+++ app/src/main/java/com/example/interac/SnapshotsActivity.java
@@ -1,17 +1,39 @@
 package com.example.interac;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
-public class SnapshotsActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class SnapshotsActivity extends AppCompatActivity {
+ImageView imageView8765;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.activity_snapshots);
+
+        imageView8765=(ImageView) findViewById(R.id.imageView8765);
+
+
+        imageView8765.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SnapshotsActivity.this, DashboardActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
