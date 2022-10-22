4dc4b7d240ea577dc170b2b8833461e6659ba808
Trilochan Reddy Sama <84437975+Trilochan-Reddy@users.noreply.github.com>
2022-10-21 22:20:03 -0500

    Updated java file for reset 1 screen

--- app/src/main/java/com/example/interac/Reset1Activity.java
+++ app/src/main/java/com/example/interac/Reset1Activity.java
@@ -3,6 +3,8 @@ package com.example.interac;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,13 @@ public class Reset1Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.activity_reset1);
         Button buttonCont514=(Button) findViewById(R.id.buttonCont514);
         buttonCont514.setOnClickListener(new View.OnClickListener() {
