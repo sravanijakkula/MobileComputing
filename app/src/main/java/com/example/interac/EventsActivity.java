945d4a62711d7ef5a975ad0e972e51476df01fdd
Trilochan Reddy Sama <84437975+Trilochan-Reddy@users.noreply.github.com>
2022-10-21 22:20:46 -0500

    Updated java file for events screen

--- app/src/main/java/com/example/interac/EventsActivity.java
+++ app/src/main/java/com/example/interac/EventsActivity.java
@@ -3,6 +3,8 @@ package com.example.interac;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,13 @@ public class EventsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.activity_events);
         ImageView imageView4=(ImageView)findViewById(R.id.imageView4);
 
