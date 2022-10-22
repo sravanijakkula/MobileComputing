41b3ce421981fa897b921f90b5bd2d09884aee8e
Trilochan Reddy Sama <84437975+Trilochan-Reddy@users.noreply.github.com>
2022-10-21 22:18:30 -0500

    Updated java file for about us screen

--- app/src/main/java/com/example/interac/AboutUsActivity.java
+++ app/src/main/java/com/example/interac/AboutUsActivity.java
@@ -3,6 +3,8 @@ package com.example.interac;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,13 @@ public class AboutUsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.activity_about_us);
 
         ImageView imageView5Back1514 = (ImageView) findViewById(R.id.imageView5Back1514);
