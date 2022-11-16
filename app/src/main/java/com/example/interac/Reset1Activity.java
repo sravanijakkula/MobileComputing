da0b899cf6503a8c5cc45dd8ef2c4d6b8a915554
Nikhila212 <97923761+Nikhila212@users.noreply.github.com>
2022-11-16 14:51:48 -0600

    Nikhila Chowdary Vaitla-S546518 Changes to the Login, Register, launch, main and reset password screens and updated the manifest xml accordingly

--- app/src/main/java/com/example/interac/Reset1Activity.java
+++ app/src/main/java/com/example/interac/Reset1Activity.java
@@ -19,16 +19,7 @@ public class Reset1Activity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-
         setContentView(R.layout.activity_reset1);
-        Button buttonCont514=(Button) findViewById(R.id.buttonCont514);
-        buttonCont514.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(Reset1Activity.this,Reset2.class);
-                startActivity(i);
-            }
 
-        });
     }
 }
