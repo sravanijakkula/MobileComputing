b30ad54df8207779a1c3364dfcbe7f2387367b0c
Nikhila212 <97923761+Nikhila212@users.noreply.github.com>
2022-11-26 20:32:20 -0600

    Nikhila Chowdary Vaitla-S546518 social media added in about us screen

--- app/src/main/java/com/example/interac/AboutUsActivity.java
+++ app/src/main/java/com/example/interac/AboutUsActivity.java
@@ -1,14 +1,19 @@
 package com.example.interac;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -17,6 +22,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class AboutUsActivity extends AppCompatActivity {
 
     TextView aboutus, quality, laptop;
+    ImageButton instagram, facebook, twitter, tiktok, linkedin, youtube;
     FirebaseFirestore fs = FirebaseFirestore.getInstance();
 
     @Override
@@ -43,6 +49,74 @@ public class AboutUsActivity extends AppCompatActivity {
                 }
             }
         });
+
+        instagram = findViewById(R.id.instagram);
+        facebook = findViewById(R.id.facebook);
+        twitter = findViewById(R.id.twitter);
+        youtube = findViewById(R.id.youtube);
+        linkedin = findViewById(R.id.linkedin);
+        tiktok = findViewById(R.id.tiktok);
+
+        instagram.setOnClickListener(new View.OnClickListener ()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent myWebLink = new Intent(Intent.ACTION_VIEW);
+                myWebLink.setData(Uri.parse("https://www.instagram.com/nwmostate/"));
+                startActivity(myWebLink);
+            }
+        });
+
+        facebook.setOnClickListener(new View.OnClickListener ()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent myWebLink = new Intent(Intent.ACTION_VIEW);
+                myWebLink.setData(Uri.parse("https://www.facebook.com/nwmissouri"));
+                startActivity(myWebLink);
+            }
+        });
+
+        twitter.setOnClickListener(new View.OnClickListener ()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent myWebLink = new Intent(Intent.ACTION_VIEW);
+                myWebLink.setData(Uri.parse("https://twitter.com/nwmostate"));
+                startActivity(myWebLink);
+            }
+        });
+
+        youtube.setOnClickListener(new View.OnClickListener ()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent myWebLink = new Intent(Intent.ACTION_VIEW);
+                myWebLink.setData(Uri.parse("https://www.youtube.com/user/NorthwestVideo"));
+                startActivity(myWebLink);
+            }
+        });
+
+        linkedin.setOnClickListener(new View.OnClickListener ()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent myWebLink = new Intent(Intent.ACTION_VIEW);
+                myWebLink.setData(Uri.parse("https://www.linkedin.com/school/northwest-missouri-state-university_2/"));
+                startActivity(myWebLink);
+            }
+        });
+
+        tiktok.setOnClickListener(new View.OnClickListener ()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent myWebLink = new Intent(Intent.ACTION_VIEW);
+                myWebLink.setData(Uri.parse("https://www.tiktok.com/@nwmostate"));
+                startActivity(myWebLink);
+            }
+        });
+
 
     }
 }
